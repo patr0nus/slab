61785040748b9316b7d840c316ee3db54eed1358
patr0nus <dk4rest@gmail.com>
2023-05-07 21:42:44 +0800

    Bump MSRV to 1.65 (which supports GATs)

--- src/list/mod.rs
+++ src/list/mod.rs
@@ -2,48 +2,30 @@ mod vec;
 
 pub use vec::VecStorage;
 
-#[cfg(not(slab_no_gat))]
 pub trait ItemMut<'a, T> {
     fn set(&mut self, item: T);
     fn get(&self) -> &T;
 }
 
-#[cfg(not(slab_no_gat))]
 pub trait ListStorage {
     type List<T>: List<Item = T>;
 }
 
-#[cfg(slab_no_gat)]
-pub trait ListStorage {}
-
-#[cfg(not(slab_no_gat))]
 pub trait ClearableListStorage: ListStorage {
     fn clear<T>(list: &mut Self::List<T>);
 }
 
-#[cfg(slab_no_gat)]
-pub trait ClearableListStorage: ListStorage {}
-
-#[cfg(not(slab_no_gat))]
 pub trait AsSliceListStorage: ListStorage {
     fn as_slice<T>(list: &Self::List<T>) -> &[T];
     fn as_mut_slice<T>(list: &mut Self::List<T>) -> &mut [T];
 }
 
-#[cfg(slab_no_gat)]
-pub trait AsSliceListStorage: ListStorage {}
-
-#[cfg(not(slab_no_gat))]
 pub trait MutRefListStorage: ListStorage {
     fn into_mut_ref<T>(
         item_mut: <<Self as ListStorage>::List<T> as List>::ItemMut<'_>,
     ) -> &'_ mut T;
 }
 
-#[cfg(slab_no_gat)]
-pub trait MutRefListStorage: ListStorage {}
-
-#[cfg(not(slab_no_gat))]
 pub trait List {
     type Item;
     type ItemMut<'a>: ItemMut<'a, Self::Item>
@@ -55,7 +37,6 @@ pub trait List {
     fn get_mut(&mut self, key: usize) -> Option<Self::ItemMut<'_>>;
 }
 
-#[cfg(not(slab_no_gat))]
 impl<'a, T> ItemMut<'a, T> for &'a mut T {
     fn set(&mut self, item: T) {
         **self = item;
--- src/list/vec.rs
+++ src/list/vec.rs
@@ -1,24 +1,19 @@
-use super::{AsSliceListStorage, ClearableListStorage, ListStorage, MutRefListStorage};
-
-#[cfg(not(slab_no_gat))]
-use super::{ItemMut, List};
+use alloc::vec::Vec;
+use super::{AsSliceListStorage, ClearableListStorage, ListStorage, MutRefListStorage, List};
 
 #[derive(Debug)]
 pub enum VecStorage {}
 
 impl ListStorage for VecStorage {
-    #[cfg(not(slab_no_gat))]
     type List<T> = Vec<T>;
 }
 
 impl ClearableListStorage for VecStorage {
-    #[cfg(not(slab_no_gat))]
     fn clear<T>(list: &mut Self::List<T>) {
         list.clear()
     }
 }
 impl MutRefListStorage for VecStorage {
-    #[cfg(not(slab_no_gat))]
     fn into_mut_ref<T>(
         item_mut: <<Self as ListStorage>::List<T> as List>::ItemMut<'_>,
     ) -> &'_ mut T {
@@ -27,17 +22,14 @@ impl MutRefListStorage for VecStorage {
 }
 
 impl AsSliceListStorage for VecStorage {
-    #[cfg(not(slab_no_gat))]
     fn as_slice<T>(list: &Self::List<T>) -> &[T] {
         list.as_slice()
     }
-    #[cfg(not(slab_no_gat))]
     fn as_mut_slice<T>(list: &mut Self::List<T>) -> &mut [T] {
         list.as_mut_slice()
     }
 }
 
-#[cfg(not(slab_no_gat))]
 impl<T> List for Vec<T> {
     type Item = T;
     type ItemMut<'a> = &'a mut T where Self: 'a;
